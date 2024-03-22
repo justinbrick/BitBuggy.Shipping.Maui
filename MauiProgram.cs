4ce0b79b351f96b30a54ef5c95ffcd1fcdb5f58c
Justin <justinbrick1@gmail.com>
2024-03-22 18:04:03 -0400

    Base for Shipping Service

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BitBuggy.Shipping.Maui;
 
@@ -18,7 +19,7 @@ public static class MauiProgram
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
-
+        builder.Services.AddSingleton<ShippingService>();
         return builder.Build();
     }
 }
---
+++ ShippingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitBuggy.Shipping.Maui;
+internal class ShippingService
+{
+    private readonly HttpClient _client = new()
+    { 
+        // FastAPI & Uvicorn server
+        BaseAddress = new Uri("https://localhost:8000")
+    };
+
+
+}
