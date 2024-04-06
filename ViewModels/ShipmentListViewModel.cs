b9423bf4532d7ba16786386be4c933a220b866b1
Rui Grier <grierr1880@forsythtech.edu>
2024-04-06 08:58:57 -0400

    observablecollection in shipmentlistviewmodel

--- ViewModels/ShipmentListViewModel.cs
+++ ViewModels/ShipmentListViewModel.cs
@@ -1,39 +1,35 @@
-using BitBuggy.Shipping.Maui.Shipping.Models;
-using Java.Util;
 using Microsoft.Identity.Client;
-using Org.Apache.Http.Authentication;
-using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BitBuggy.Shipping.Maui.ViewModels;
 
 class ShipmentListViewModel : INotifyPropertyChanged
 {
-    private readonly IPublicClientApplication _clientApplication;
+    private readonly IPublicClientApplication? _clientApplication;
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
-
+    private ObservableCollection<Shipment>? _shipments;
+    public ObservableCollection<Shipment> Shipments
+    {
+        get => _shipments;
+        set
+        {
+            if (_shipments != value)
+            {
+                _shipments = value;
+                OnPropertyChanged(nameof(Shipments));
+            }
+        }
+    }
 }
 
 class Shipment
 {
-    private ShipmentStatusViewModel? shipmentStatus;
-    private UUID? shippingId;
-    private string? shippingAddress;
-    private string? provider;
-    private string? providerShipmentID;
-    private DateTime? createdDate;
-    private DateTime? deliveredDate;
-
-    public UUID? ShippingId { get => shippingId; set => shippingId = value; }
-    public string? ShippingAddress { get => shippingAddress; set => shippingAddress = value; }
-    public string? Provider { get => provider; set => provider = value; }
-    public string? ProviderShipmentID { get => providerShipmentID; set => providerShipmentID = value; }
-    public DateTime? CreatedDate { get => createdDate; set => createdDate = value; }
-    public DateTime? DeliveredDate { get => deliveredDate; set => deliveredDate = value; }
+    
 }
